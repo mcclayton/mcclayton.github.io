3eeb3c7b1f3224d43aeed8a8a1a9d66d95f33099
Michael Clayton <michael.clayton@procore.com>
2021-12-23 10:03:58 -0800

    Add tropical page

---
+++ src/pages/tropical.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Layout from '../components/layout';
+
+export default class TropicalPage extends Component {
+  render() {
+    return (
+      <Layout>
+        <div className="tropical" id="bg" />
+      </Layout>
+    );
+  }
+}
